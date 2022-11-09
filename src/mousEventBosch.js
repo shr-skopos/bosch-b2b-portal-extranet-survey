e90f616b078a2e16e09f4de3f6c20da5ad30dfb0
shr-skopos <saied.hosseinian@skopos-connect.de>
2022-11-09 13:04:57 +0100

    add mouseEvent

--- src/mousEventBosch.js
+++ src/mousEventBosch.js
@@ -1,3 +1,10 @@
 export default function mousEventBosch() {
-    console.log("Marc & Saied");
+    let choiceRow;
+    choiceRow = document.querySelectorAll("tbody tr .c1");
+
+    for (let index = 0; index < document.querySelectorAll(".ChoiceRow .table-cell span").length; index++) {
+        choiceRow[index].addEventListener("mousemove", function (e) {
+            console.log(this)
+        })
+    }
 }
