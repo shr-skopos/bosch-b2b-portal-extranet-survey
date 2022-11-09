ec2e4de816ae9ecdf79b7417956e9e66b4e621ca
shr-skopos <saied.hosseinian@skopos-connect.de>
2022-11-09 13:08:13 +0100

    add mouse Position

--- src/mousEventBosch.js
+++ src/mousEventBosch.js
@@ -2,9 +2,21 @@ export default function mousEventBosch() {
     let choiceRow;
     choiceRow = document.querySelectorAll("tbody tr .c1");
 
+    const choisImages = (elem) => {
+        let elemPosition = Number(elem.id.split("~").slice(2).join());
+        let img = document.createElement("img");
+        img.className = "img-top";
+        console.log(elemPosition);
+
+        elem.addEventListener("mouseout", () => {
+            console.log("out");
+        })
+        choiceRow[elemPosition].insertBefore(img, choiceRow[elemPosition][elemPosition]);
+    }
+
     for (let index = 0; index < document.querySelectorAll(".ChoiceRow .table-cell span").length; index++) {
         choiceRow[index].addEventListener("mousemove", function (e) {
-            console.log(this)
+            choisImages(this)
         })
     }
 }
